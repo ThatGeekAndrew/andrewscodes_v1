7947fe8e48774ada7bc816adbef7b0dc6e03c57c
geeksmarter <andrewalsante@gmail.com>
2017-04-04 22:44:38 -0400

    3rd times the charm grunt and font awesome

--- Gruntfile.js
+++ Gruntfile.js
@@ -434,8 +434,8 @@ module.exports = function (grunt) {
           dest: '<%= yeoman.dist %>'
         }, {
           expand: true,
-          cwd: 'bower_components/font-awesome/*',
-          src: 'fonts/*',
+          cwd: './bower_components/font-awesome/fonts',
+          src: '{,*/}*.*',
           dest: '<%= yeoman.dist %>/fonts'
         }]
       },
