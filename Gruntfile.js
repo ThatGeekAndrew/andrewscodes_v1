2524ce3157943aa47ad6aa6e93cdf4ee10e4f079
geeksmarter <andrewalsante@gmail.com>
2017-04-04 22:33:11 -0400

    updating gruntfile and some markup

--- Gruntfile.js
+++ Gruntfile.js
@@ -434,9 +434,9 @@ module.exports = function (grunt) {
           dest: '<%= yeoman.dist %>'
         }, {
           expand: true,
-          cwd: 'bower_components/fontawesome/',
+          cwd: 'bower_components/font-awesome/*',
           src: 'fonts/*',
-          dest: '<%= yeoman.dist %>'
+          dest: '<%= yeoman.dist %>/fonts'
         }]
       },
       styles: {
