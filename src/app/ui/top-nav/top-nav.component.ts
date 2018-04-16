b69b03b931f4d5bd76f121b57af2650f97feb4ae
geeksmarter <andrewalsante@gmail.com>
2018-04-16 07:19:03 -0400

    Using ng-bootstrap to display modal and log user out

--- src/app/ui/top-nav/top-nav.component.ts
+++ src/app/ui/top-nav/top-nav.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+
 import { AuthService } from '../../core/auth.service';
 
 @Component({
@@ -8,10 +10,34 @@ import { AuthService } from '../../core/auth.service';
   styleUrls: ['./top-nav.component.scss'],
 })
 export class TopNavComponent {
+  closeResult: string;
 
-  constructor(public auth: AuthService) { }
+  constructor(public auth: AuthService, private modalService: NgbModal) { }
 
-  logout() {
-    this.auth.signOut();
+  open(content: any) {
+    this.modalService.open( content, {
+      size: 'sm',
+      centered: true,
+      windowClass: 'animated fadeIn',
+      // backdropClass: 'blurred'
+    }).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+
+      if (result == 'logout') {
+        this.auth.signOut();
+      }
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
   }
 }
